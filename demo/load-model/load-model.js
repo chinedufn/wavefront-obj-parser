c235b808e06e6d7b1af3f5f8997ee58735fa4ccb
Chinedu Francis Nwafili <frankie.nwafili@gmail.com>
2016-08-02 10:15:15 -0400

    Expand vertex indices
    Allows us to use multiple uvs per vertex (depending on which triangle we are drawing)
    
    Need to pull this out into a separate tested module

--- demo/load-model/load-model.js
+++ demo/load-model/load-model.js
@@ -19,15 +19,12 @@ function LoadModel (gl) {
   var shaderObj = initShader(gl)
 
   var vertexPositionBuffer = gl.createBuffer()
+  var vertexTextureBuffer = gl.createBuffer()
   var vertexPositionIndexBuffer = gl.createBuffer()
-
-  gl.bindBuffer(gl.ARRAY_BUFFER, vertexPositionBuffer)
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(modelJSON.vertex), gl.STATIC_DRAW)
 
   var vertexIndices = []
 
   // TODO: Less verbose
-  // TODO: Only expand if face has 4 vertices
   for (var i = 0; i < modelJSON.vertexIndex.length / 4; i++) {
     vertexIndices.push(modelJSON.vertexIndex[i * 4])
     vertexIndices.push(modelJSON.vertexIndex[i * 4 + 1])
@@ -40,8 +37,48 @@ function LoadModel (gl) {
     }
   }
 
+  // Dedupe vertex indices..
+  //  since we can't have multiple VBO's for texture and normals coords
+  //  we have to repeat some of the data for when a vertex needs multiple UVs / normals
+  // TODO: Pull into own tested module with README
+  var vertexPositionIndices = []
+  var vertexUVs = []
+  var encounteredIndices = {}
+  var largestPositionIndex = 0
+  vertexIndices.forEach(function (vertexIndex, counter) {
+    largestPositionIndex = Math.max(largestPositionIndex, vertexIndex)
+    // If this is our first time seeing the vertex index we add it
+    // Later we'll add all of the duplicate indices into the end of the array
+    if (!encounteredIndices[vertexIndex]) {
+      vertexPositionIndices[counter] = vertexIndex
+      // Push the appropriate UV coordinates
+      for (var i = 0; i < 2; i++) {
+        vertexUVs[vertexIndex * 2 + i] = modelJSON.uv[modelJSON.uvIndex[counter] * 2 + i]
+      }
+      encounteredIndices[vertexIndex] = true
+    }
+  })
+  vertexIndices.forEach(function (vertexIndex, counter) {
+    // Add all of the duplicate indices that we skipped over above
+    if (encounteredIndices[vertexIndex]) {
+      vertexPositionIndices[counter] = ++largestPositionIndex
+      for (var i = 0; i < 3; i++) {
+        if (i < 2) {
+          vertexUVs[largestPositionIndex * 2 + i] = modelJSON.uv[modelJSON.uvIndex[counter] * 2 + i]
+        }
+        modelJSON.vertex[largestPositionIndex * 3 + i] = modelJSON.vertex[vertexIndex * 3 + i]
+      }
+    }
+  })
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexPositionBuffer)
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(modelJSON.vertex), gl.STATIC_DRAW)
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexTextureBuffer)
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexUVs), gl.STATIC_DRAW)
+
   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, vertexPositionIndexBuffer)
-  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(vertexIndices), gl.STATIC_DRAW)
+  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(vertexPositionIndices), gl.STATIC_DRAW)
 
   function draw (gl, opts) {
     var modelPosition = [2.0, -2.0, -10.0]
@@ -60,7 +97,7 @@ function LoadModel (gl) {
     gl.uniformMatrix4fv(shaderObj.pMatrixUniform, false, opts.pMatrix)
     gl.uniformMatrix4fv(shaderObj.mvMatrixUniform, false, modelMatrix)
 
-    gl.drawElements(gl.TRIANGLES, vertexIndices.length, gl.UNSIGNED_SHORT, 0)
+    gl.drawElements(gl.TRIANGLES, vertexPositionIndices.length, gl.UNSIGNED_SHORT, 0)
   }
 
   return {draw: draw}
